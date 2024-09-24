26044d67c8a1db9c197dd2c27bbf8bc60a187039
rugcer04 <rugilecerniauskaite@gmail.com>
2024-09-24 17:20:10 +0300

    Pridedamas duomenų įvedimas, išvedimas ir funkcija apskaičiuoti galutinį pažymį su vidurkiu.

--- Lib.h
+++ Lib.h
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using std::string;
 using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::setprecision;
+using std::fixed;
+using std::setw;
--- pradine.cpp
+++ pradine.cpp
@@ -1,4 +1,4 @@
-#include "Lib.h";
+#include "Lib.h"
 
 //struktura studento duomenims saugoti
 struct Studentas {
@@ -8,3 +8,64 @@ struct Studentas {
    int egzaminas;
    double galutinis;
 };
+
+//funkcija galutiniam balui apskaiciuoti naudojant vidurki
+double skaiciuotiGalutiniVidurkiu(const vector<int> namuDarbai, int egzaminas) {
+   double vidurkis = 0.0;
+   for (int nd: namuDarbai) {
+      vidurkis += nd;
+   }
+
+   vidurkis /= namuDarbai.size();
+
+   return vidurkis * 0.4 + egzaminas * 0.6;
+}
+
+//funkcija ivesti duomenims
+void ivedimas(Studentas &Lok) {
+   cout << "Įveskite studento vardą: " << endl;
+   cin >> Lok.vardas;
+   cout << "Įveskite studento pavardę: " << endl;
+   cin >> Lok.pavarde;
+
+   int ndSk;
+   cout << "Įveskite namų darbų skaičių: " << endl;
+   cin >> ndSk;
+
+   Lok.namuDarbai.resize(ndSk);
+   for (int j = 0; j < ndSk; ++j) {
+      cout << "Įveskite " << j+1 << "-ojo namų darbo pažymį: " << endl;
+      cin >> Lok.namuDarbai[j];
+   }
+
+   cout << "Įveskite egzamino rezultatą: " << endl;
+   cin >> Lok.egzaminas;
+
+   Lok.galutinis = skaiciuotiGalutiniVidurkiu(Lok.namuDarbai, Lok.egzaminas);
+}
+
+//funkcija isvesti duomenis
+void isvedimas(const vector<Studentas>& studentai) {
+   cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis (Vid.)" << endl;
+   cout << "---------------------------------------------" << endl;
+   for (const auto& Lok: studentai) {
+      cout << left << setw(15) << Lok.pavarde << setw(15) << Lok.vardas << fixed << setprecision(2) << Lok.galutinis  << endl;
+   }
+}
+
+int main() {
+   cout << "Įveskite studentų skaičių: " << endl;
+    int studentuSk;
+    cin >> studentuSk;
+
+    vector<Studentas> studentai(studentuSk);
+
+    for (int i = 0; i < studentuSk; i++) {
+        ivedimas(studentai[i]);
+    }
+
+    isvedimas(studentai);
+
+    return 0;
+
+}
