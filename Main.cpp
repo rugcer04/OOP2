9f4c3b68296a89bf5d38028f2e90d70357285e98
rugcer04 <rugilecerniauskaite@gmail.com>
2024-10-21 21:12:45 +0300

    Pridedamas pasirinkimas dirbti su list.

--- Main.cpp
+++ Main.cpp
@@ -3,124 +3,120 @@
 #include "Timer.h"
 
 int main() {
-    /*
-    //TEST
     char konteinerioPasirinkimas;
     cout << "Norite naudoti vector (V) ar list (L)? ";
     cin >> konteinerioPasirinkimas;
     konteinerioPasirinkimas = toupper(konteinerioPasirinkimas);
 
-    if (konteinerioPasirinkimas == 'L') {
-        list<Studentas> studentai;
-        string failoPavadinimas;
-        cout << "Įveskite failo pavadinimą: ";
-        cin >> failoPavadinimas;
-        nuskaitytiIsFailo(studentai, failoPavadinimas);
-
-        string failoPavadinimasRez = "studrez.txt";
-        char pasirinkimas = 'V';
-
-        isvedimasIFaila(studentai, pasirinkimas, failoPavadinimasRez);
-
-    } else if (konteinerioPasirinkimas == 'V') {
-    */
-
-
-    vector<Studentas> studentai;
-    char duomenuIvedimoBudas = pasirinktiDuomenuIvedima();
+    //Darbas su vektoriumi
+    if (konteinerioPasirinkimas == 'V') {
+        vector<Studentas> studentai;
+        char duomenuIvedimoBudas = pasirinktiDuomenuIvedima();
     
-    //Ivesti rankiniu budu
-    if (duomenuIvedimoBudas == 'I') {
-        ivedimasRanka(studentai);
-        /*
-        cout << "Įveskite studentų skaičių: ";
-        cin.ignore();
-        string input;
-        int studentuSk;
+        //Ivesti rankiniu budu
+        if (duomenuIvedimoBudas == 'I') {
+            ivedimasRanka(studentai);
+        }
 
-        while(true) {
-            getline(cin, input);
+        //Nuskaitymas is failo
+        else if (duomenuIvedimoBudas == 'F') {
+            //system("dir *.txt");
+            string failoPavadinimas;
+            cout << "Įveskite failo pavadinimą: ";
+            cin >> failoPavadinimas;
 
-            try{
-                stringstream ss(input);
-                if (!(ss >> studentuSk)) {
-                    throw invalid_argument("įvestis nėra skaičius. ");
-                }
-                break;
-            } catch (const invalid_argument &e){
-                cout << "Klaida: " << e.what() << "Bandykite dar kartą\n";
+            Timer t1;
+            nuskaitytiIsFailo(studentai, failoPavadinimas);
+            cout << "Failo su " << studentai.size() << " įrašų nuskaitymo laikas: " << t1.elapsed() << " s\n" << endl;
+        }
+
+        //Failu generavimas
+        else if (duomenuIvedimoBudas == 'G') {
+            vector<int> studentuDydziai = {1000, 10000, 100000, 1000000, 10000000};
+            char pasirinkimas;
+
+            for (int n : studentuDydziai) {
+                studentai.clear();
+                string failoPavadinimas = "studentai" + to_string(n) + ".txt";
+
+                Timer t2;
+                generuotiFaila(n, failoPavadinimas);
+                cout << "Failo su " << n << " įrašų generavimo laikas: " << t2.elapsed() << " s\n" << endl;        
             }
+
+        return 0;
         }
 
-        studentai.resize(studentuSk);
-
-        for (int i = 0; i < studentuSk; i++) {
-            pasirinktiDuomenuIvedimoBuda(studentai[i]);
-        }
-        */
-
-    }
-    //Nuskaitymas is failo
-     else if (duomenuIvedimoBudas == 'F') {
-        //cin.ignore();
-        //system("dir *.txt");
-        string failoPavadinimas;
-        cout << "Įveskite failo pavadinimą: ";
-        cin >> failoPavadinimas;
-
-        Timer t1;
-        nuskaitytiIsFailo(studentai, failoPavadinimas);
-        cout << "Failo su " << studentai.size() << " įrašų nuskaitymo laikas: " << t1.elapsed() << " s\n" << endl;
-    }
-
-    //failo generavimas
-    else if (duomenuIvedimoBudas == 'G') {
-        vector<int> studentuDydziai = {1000, 10000, 100000, 1000000, 10000000};
-        char pasirinkimas;
-
-        for (int n : studentuDydziai) {
-            studentai.clear();
-            string failoPavadinimas = "studentai" + to_string(n) + ".txt";
-
-            Timer t2;
-            generuotiFaila(n, failoPavadinimas);
-            cout << "Failo su " << n << " įrašų generavimo laikas: " << t2.elapsed() << " s\n" << endl;        
+        //Pasirenkame galutinio pazymio skaiciavimo metoda
+        char pasirinkimas = pasirinktiGalutinioskaiciavimoMetoda();
+        for (Studentas& studentas : studentai) {
+            skaiciuotiGalutini(studentas, pasirinkimas);
         }
 
-    return 0;
-    }
+        //Pasirenkame studentu rusiavimo parametra
+        pasirinktiRusiavimoParametra(studentai);
 
-    //Pasirenkame galutinio pazymio skaiciavimo metoda
-    char pasirinkimas = pasirinktiGalutinioskaiciavimoMetoda();
-    for (Studentas& studentas : studentai) {
-        skaiciuotiGalutini(studentas, pasirinkimas);
-    }
+        //Skirstyti studentus i vargsiukus ir kietiakus
+        Timer t3;
+        vector<Studentas> vargsiukai, kietiakai;
+        skirstytiStudentus(studentai, vargsiukai, kietiakai);
+        cout << "Failo su " << studentai.size() << " įrašų surūšiavimo į dvi grupes laikas: " << t3.elapsed() << " s\n" << endl;
 
-    //Pasirenkame studentu rusiavimo parametra
-    pasirinktiRusiavimoParametra(studentai);
+        Timer t4;
+        //Isvesti vargsiukus i faila
+        isvedimasIFaila(vargsiukai, pasirinkimas, "vargsiukai.txt");
+        //Isvesti kietiakus i faila
+        isvedimasIFaila(kietiakai, pasirinkimas, "kietiakai.txt");
+        cout << "Failo su " << studentai.size() << " įrašų išvedimo į failus laikas: " << t4.elapsed() << " s\n" << endl;
 
-    //Skirstyti studentus i vargsiukus ir kietiakus
-    Timer t3;
-    vector<Studentas> vargsiukai, kietiakai;
-    skirstytiStudentus(studentai, vargsiukai, kietiakai);
-    cout << "Failo su " << studentai.size() << " įrašų surūšiavimo į dvi grupes laikas: " << t3.elapsed() << " s\n" << endl;
+        //pasirenkame kaip pateikti rezultatus (faile ar terminale)
+        if (duomenuIvedimoBudas == 'I') {
+            pasirinktiRezultatuIsvedimoBuda(studentai, pasirinkimas);
+        } else {
+            string failoPavadinimas = "studrez.txt";
+            isvedimasIFaila(studentai, pasirinkimas, failoPavadinimas);
+        }
 
-    Timer t4;
-    //Isvesti vargsiukus i faila
-    isvedimasIFaila(vargsiukai, pasirinkimas, "vargsiukai.txt");
-    //Isvesti kietiakus i faila
-    isvedimasIFaila(kietiakai, pasirinkimas, "kietiakai.txt");
-    cout << "Failo su " << studentai.size() << " įrašų išvedimo į failus laikas: " << t4.elapsed() << " s\n" << endl;
+    //Darbas su sarasu
+    } else if (konteinerioPasirinkimas == 'L') {
+        vector<Studentas> studentai;
+        char duomenuIvedimoBudas = pasirinktiDuomenuIvedima();
+    
+        //Ivesti rankiniu budu
+        if (duomenuIvedimoBudas == 'I') {
+            ivedimasRanka(studentai);
+        }
 
-    //pasirenkame kaip pateikti rezultatus (faile ar terminale)
-    if (duomenuIvedimoBudas == 'I') {
-        pasirinktiRezultatuIsvedimoBuda(studentai, pasirinkimas);
-    } else {
-        string failoPavadinimas = "studrez.txt";
-        isvedimasIFaila(studentai, pasirinkimas, failoPavadinimas);
+        //Nuskaitymas is failo
+        else if (duomenuIvedimoBudas == 'F') {
+            //system("dir *.txt");
+            string failoPavadinimas;
+            cout << "Įveskite failo pavadinimą: ";
+            cin >> failoPavadinimas;
+
+            Timer t1;
+            nuskaitytiIsFailo(studentai, failoPavadinimas);
+            cout << "Failo su " << studentai.size() << " įrašų nuskaitymo laikas: " << t1.elapsed() << " s\n" << endl;
+        }
+
+        //Failu generavimas
+        else if (duomenuIvedimoBudas == 'G') {
+            list<int> studentuDydziai = {1000, 10000, 100000, 1000000, 10000000};
+            char pasirinkimas;
+
+            for (int n : studentuDydziai) {
+                studentai.clear();
+                string failoPavadinimas = "studentai" + to_string(n) + ".txt";
+
+                Timer t2;
+                generuotiFaila(n, failoPavadinimas);
+                cout << "Failo su " << n << " įrašų generavimo laikas: " << t2.elapsed() << " s\n" << endl;        
+            }
+
+        return 0;
+        }
     }
     
-
 
     return 0;
 }
