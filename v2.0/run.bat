2e0374c97452bab2dbdc8f2e5b9ca55b6ae24f7b
rugcer04 <rugilecerniauskaite@gmail.com>
2024-12-12 20:29:02 +0200

    Pridedami CMakelists.txt ir run.bat failai.

--- v2.0/run.bat
+++ v2.0/run.bat
@@ -1,6 +1,11 @@
-cmake -S . -B Release -G "MinGW Makefiles"
-cmake --install Release
+cmake -S . -B Release -G "MinGW Makefiles" 
 cmake --build Release
+cmake --install Release
+
 cd Release
+
+runTests.exe
+pause
+
 Studentai.exe
 pause
